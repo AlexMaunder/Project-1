847cbfa20fa46a3a00f0c56c5cf4fa46ac33604e
alexmaunder <alex_maunder@hotmail.com>
2020-06-23 13:38:33 +1000

    enabled user login & admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   private
   def fetch_user
-    @current_user = User.find_by :id => session[:user] if session[:user_id].present?
-    session[:id] = nil unless @current_user.present?
+    @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
+    session[:user_id] = nil unless @current_user.present?
   end
 
   def check_for_login
